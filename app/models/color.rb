1322e1f79be36fadcf01fc11cb3059272f44372b
thisisnaty <garcia.doe@gmail.com>
2015-04-10 20:17:46 -0500

    Javascript del color

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,12 +1,18 @@
 # Model Color
 # Table colors
 class Color < ActiveRecord::Base
+  before_validation :add_style
   after_validation :titleize_name
 
   validates :name, presence: true
   validates :hex_code, presence: true
+  validates :style, presence: true
 
   private
+
+  def add_style
+  	self.style = "background-color: " + hex_code
+  end
 
   def titleize_name
     self.name = name.mb_chars.downcase.to_s.titleize
